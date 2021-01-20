a6bd0836924cad383750eaeb566b7d7119d36699
Standley Lin <standleylin@gmail.com>
2021-01-20 14:53:02 +0800

    add chrome developer tools

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,13 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 import { rootEpic, rootReducer } from './modules/root';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const epicMiddleware = createEpicMiddleware();
 
 export default function configureStore() {
     const store = createStore(
         rootReducer,
-        applyMiddleware(epicMiddleware)
+        composeWithDevTools(
+            applyMiddleware(epicMiddleware)
+        )
     );
 
     epicMiddleware.run(rootEpic);
